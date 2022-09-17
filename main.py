375f08824c7f85b02a306171a0f9a1f920d58c29
Juan Mariano Mariano <>
2022-09-17 00:05:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+print("Holaaaaaa")
+print("Bienvenido a mi trivia sobre los animales") 
+nombre= input("Ingrese su nombre o apodo: \n ")
+print("\n Holaa", nombre,"a continuación deberas responder las siguientes preguntas escribiendo la letra correspondiente a las opciones de respuesta y presionando ¨enter¨ para enviar tu respuesta: \n")
+print("1- ¿Cual es el animal mas rapido del mundo?\n")
+print(" a- Tigre")
+print(" b- Jaguar")
+print(" c- Puma")
+print(" d- Guepardo")
+respuesta_usuario=input("Escriba su respuesta :")
+while respuesta_usuario  not in ("a","b","c","d"): 
+ 
+   respuesta_usuario = input("Debes responder con a,b,c,d.ingresa nuevamente tu respuesta : ")
+if respuesta_usuario == "d" :
+   print("correcto")
+else: 
+ print("incorrecto")
+print("1- ¿Cual es el animal mas rapido del mundo acuatico?\n")
+print(" a- wahoo")
+print(" b- Marlin azul")
+print(" c- Tiburon Mako")
+print(" d- Pez Martillo")
+respuesta_usuario=input("Escriba su respuesta :")
+while respuesta_usuario  not in ("a","b","c","d"): 
+ 
+   respuesta_usuario = input("Debes responder con a,b,c,d.ingresa nuevamente tu respuesta : ")
+if respuesta_usuario == "c" :
+   print("correcto")
+else: 
+ print("incorrecto")
+while respuesta_usuario  not in ("a","b","c","d"): 
+ 
+   respuesta_usuario = input("Debes responder con a,b,c,d.ingresa nuevamente tu respuesta : ")
+if respuesta_usuario == "d" :
+   print("correcto")
+else: 
+ print("incorrecto")
+print("1- ¿Cual es el animal mas grande del oceano?\n")
+print(" a- La ballena de aleta")
+print(" b- El gusano “cordón de bota”")
+print(" c- Medusa melena de leon")
+print(" d- Orca")
+respuesta_usuario=input("Escriba su respuesta :")
+while respuesta_usuario  not in ("a","b","c","d"): 
+ 
+   respuesta_usuario = input("Debes responder con a,b,c,d.ingresa nuevamente tu respuesta : ")
+if respuesta_usuario == "b" :
+   print("correcto")
+else: 
+ print("incorrecto")
